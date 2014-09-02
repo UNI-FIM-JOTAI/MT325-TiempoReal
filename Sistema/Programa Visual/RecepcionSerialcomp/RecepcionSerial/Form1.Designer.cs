755451d79080dd771b0402fea8a74d0e58d81ad1
Juan Ignacio Basilio Flores <juan.basflo.initec@gmail.com>
2014-09-02 17:01:25 -0500

    sistema de comunicación
    listo para enviar palabras byte por byte

--- Sistema/Programa Visual/RecepcionSerialcomp/RecepcionSerial/Form1.Designer.cs
+++ Sistema/Programa Visual/RecepcionSerialcomp/RecepcionSerial/Form1.Designer.cs
@@ -38,12 +38,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Location = new System.Drawing.Point(23, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
@@ -51,21 +57,22 @@
             // 
             // serialPort1
             // 
-            this.serialPort1.PortName = "COM4";
+            this.serialPort1.PortName = "COM23";
             // 
             // Monitor
             // 
-            this.Monitor.Location = new System.Drawing.Point(26, 36);
+            this.Monitor.Location = new System.Drawing.Point(26, 25);
             this.Monitor.Multiline = true;
             this.Monitor.Name = "Monitor";
-            this.Monitor.Size = new System.Drawing.Size(195, 118);
+            this.Monitor.Size = new System.Drawing.Size(195, 20);
             this.Monitor.TabIndex = 1;
+            this.Monitor.TextChanged += new System.EventHandler(this.Monitor_TextChanged);
             // 
             // Btn_Salir
             // 
-            this.Btn_Salir.Location = new System.Drawing.Point(172, 223);
+            this.Btn_Salir.Location = new System.Drawing.Point(336, 278);
             this.Btn_Salir.Name = "Btn_Salir";
-            this.Btn_Salir.Size = new System.Drawing.Size(96, 27);
+            this.Btn_Salir.Size = new System.Drawing.Size(96, 23);
             this.Btn_Salir.TabIndex = 2;
             this.Btn_Salir.Text = "&Salir";
             this.Btn_Salir.UseVisualStyleBackColor = true;
@@ -73,7 +80,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 225);
+            this.button1.Location = new System.Drawing.Point(26, 278);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -83,7 +90,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(172, 197);
+            this.textBox1.Location = new System.Drawing.Point(114, 215);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
@@ -92,7 +99,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 197);
+            this.label2.Location = new System.Drawing.Point(37, 218);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 5;
@@ -100,26 +107,85 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(155, 160);
+            this.textBox2.Location = new System.Drawing.Point(114, 178);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 167);
+            this.label3.Location = new System.Drawing.Point(37, 181);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "prueb";
+            this.label3.Text = "Dato de envio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(325, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(23, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(325, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(23, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tamaño recibdo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tamaño del dato";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Datos Anteriores";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(462, 326);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -147,6 +213,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
---
+++ Sistema/Programa Visual/RecepcionSerialcomp/RecepcionSerial/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace RecepcionSerial
+{
+    public partial class Form1 : Form
+    {
+        string Recibirdato;
+        string Enviardato;
+        string temp_char;
+        string mostrar_envio;
+        int tam_s;
+        int num_list = 0;
+
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            if(!serialPort1.IsOpen)
+            {try
+                {
+                    serialPort1.Open();
+                
+            }
+                catch(System.Exception ex)
+            {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            //serialPort1.DataReceived+= new System.IO.Ports.SerialDataReceivedEventHandler(this.Recibir);
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Recibir);
+        }
+
+
+        private void extract_send()
+        {
+            tam_s = Enviardato.Length;
+            for (int i = tam_s-1; i >=0;i--)
+            {
+                temp_char = Enviardato.Remove(0, i);
+                serialPort1.Write(temp_char);
+                Enviardato = Enviardato.Remove(i);
+                
+            }
+        }
+        private void Recibir(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+           // Recibirdato = "";
+            Recibirdato += this.serialPort1.ReadExisting();              
+            this.Invoke(new EventHandler(actualizar));
+        }
+
+        private void actualizar (object sender, EventArgs e)
+        {   
+            //int IniAN0 = Recibirdato.IndexOf("w");
+            //int FiAN0 = Recibirdato.IndexOf("x");
+            int Tamanio;
+            Tamanio = Recibirdato.Length;
+            //if (Recibirdato.Length > 5)
+            //{
+            //Recibirdato = "";
+            //}
+            //Monitor.Text = "";
+            //Monitor.Text = ("dato recibido = " + Recibirdato + " posicion de la w en " + IniAN0 + "posición de la x en " + FiAN0 + "longitud del texto = " + Tamanio);
+           
+            Monitor.Text = "";
+            Monitor.Text = (Recibirdato);
+
+            
+            //textBox4.Text = Tamanio.ToString();   
+            textBox4.Text = Convert.ToString(Tamanio);  
+            //Monitor.Text = Recibirdato;
+            //Recibirdato = "";
+        }
+
+        private void Btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Recibirdato = "";
+            //serialPort1.Open(); //ya estaba abierto el puerto :p
+            Enviardato = (Convert.ToString(textBox1.Text));
+            mostrar_envio = Enviardato;
+            //serialPort1.Write(Enviardato.Remove(0,2));//this we are sending by RS232
+                //
+                tam_s = Enviardato.Length;
+                if (tam_s != 0)
+                {
+                    for (int i = 1; i < tam_s; i++)
+                    {
+                        temp_char = Enviardato.Remove(i);
+                        temp_char = temp_char.Remove(0, i - 1);
+                        serialPort1.Write(temp_char);
+                        Thread.Sleep(200);
+
+                    }
+                    temp_char = Enviardato.Remove(0, tam_s - 1);
+                    serialPort1.Write(temp_char);
+                    if (Monitor.Text.Length != 0)
+                    {
+                        listBox1.Items.Insert(num_list, Convert.ToString(Monitor.Text));
+                        num_list++;
+                    }
+                }
+                //
+            textBox2.Text = (mostrar_envio);
+            textBox3.Text = (Convert.ToString(mostrar_envio.Length));
+            //textBox1.Text = (Convert.ToString(0x02));//now we will see what we be sent to the  pic
+           // serialPort1.Close();
+                      
+         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //solo para redacción
+            
+           
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Monitor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+
+
+
+        }
+
+        
+    }
+
